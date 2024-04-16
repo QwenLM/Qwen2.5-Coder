fc255e1b3202ea2924a2041e47660828d0fa4b9d
cyente <cuizeyu15@gmail.com>
2024-04-16 23:11:53 +0800

    Create CodeQwen1.5-base-fim.py

---
+++ examples/CodeQwen1.5-base-fim.py
@@ -0,0 +1,24 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+# load model
+device = "cuda" # the device to load the model onto
+
+TOKENIZER = AutoTokenizer.from_pretrained("Qwen/CodeQwen1.5-7B")
+MODEL = AutoModelForCausalLM.from_pretrained("Qwen/CodeQwen1.5-7B", device_map="auto").eval()
+
+input_text = """<fim_prefix>def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    <fim_suffix>
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)<fim_middle>"""
+
+model_inputs = TOKENIZER([input_text], return_tensors="pt").to(device)
+
+# Use `max_new_tokens` to control the maximum output length.
+generated_ids = MODEL.generate(model_inputs.input_ids, max_new_tokens=512, do_sample=False)[0]
+# The generated_ids include prompt_ids, we only need to decode the tokens after prompt_ids.
+output_text = TOKENIZER.decode(generated_ids[len(model_inputs.input_ids[0]):], skip_special_tokens=True)
+
+print(f"Prompt: {input_text}\n\nGenerated text: {output_text}")
