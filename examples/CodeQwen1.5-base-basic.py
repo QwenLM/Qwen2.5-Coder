72a6114b353ca8d3384ce75c88163fc055787e1e
cyente <cuizeyu15@gmail.com>
2024-04-22 16:32:25 +0800

    Update CodeQwen1.5-base-basic.py
    lower variable tokenizer model

--- examples/CodeQwen1.5-base-basic.py
+++ examples/CodeQwen1.5-base-basic.py
@@ -2,17 +2,17 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 device = "cuda" # the device to load the model onto
 
 # Now you do not need to add "trust_remote_code=True"
-TOKENIZER = AutoTokenizer.from_pretrained("Qwen/CodeQwen1.5-7B")
-MODEL = AutoModelForCausalLM.from_pretrained("Qwen/CodeQwen1.5-7B", device_map="auto").eval()
+tokenizer = AutoTokenizer.from_pretrained("Qwen/CodeQwen1.5-7B")
+model = AutoModelForCausalLM.from_pretrained("Qwen/CodeQwen1.5-7B", device_map="auto").eval()
 
 
 # tokenize the input into tokens
 input_text = "#write a quick sort algorithm"
-model_inputs = TOKENIZER([input_text], return_tensors="pt").to(device)
+model_inputs = tokenizer([input_text], return_tensors="pt").to(device)
 
 # Use `max_new_tokens` to control the maximum output length.
-generated_ids = MODEL.generate(model_inputs.input_ids, max_new_tokens=1024, do_sample=False)[0]
+generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=1024, do_sample=False)[0]
 # The generated_ids include prompt_ids, so we only need to decode the tokens after prompt_ids.
-output_text = TOKENIZER.decode(generated_ids[len(model_inputs.input_ids[0]):], skip_special_tokens=True)
+output_text = tokenizer.decode(generated_ids[len(model_inputs.input_ids[0]):], skip_special_tokens=True)
 
 print(f"Prompt: {input_text}\n\nGenerated text: {output_text}")
