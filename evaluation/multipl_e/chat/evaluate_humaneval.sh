da460460bb0a2e4619340b6225d3298cae71c4cb
CSJianYang <506733299@qq.com>
2024-05-15 16:53:00 +0800

    Update evaluate_humaneval.sh
    Add tensor_parallel_size for Llama3-70B

--- evaluation/multipl_e/chat/evaluate_humaneval.sh
+++ evaluation/multipl_e/chat/evaluate_humaneval.sh
@@ -1,11 +1,19 @@
-export PATH=/python/bin/:$PATH;
-MODEL_DIR="Qwen/CodeQwen1.5-7B-Chat"
-DATA_PATH="./data/"
-LANGUAGE=${1}
+export PATH=/cpfs01/shared/public/yj411294/miniconda3/envs/qwen/bin:$PATH;
+export TOKENIZERS_PARALLELISM=true;
+MODEL_DIR=${1}
+MODEL_DIR=${MODEL_DIR:-"/home/data/dangkai.dk/hf_ckpts/7b.5000B--longcnt_mixv1-base1000w-cpt256k-v10.15.26_code_v6"}
+DATA_PATH="/cpfs01/shared/public/yj411294/CrossMutation/CodeQwen1.5/evaluation/multipl_e/chat/data/"
+LANGUAGE=${2}
 LANGUAGE=${LANGUAGE:-"python"}
 echo "benchmark: humaneval, language ${LANGUAGE}"
-GENERATION_PATH="./results/generation_${LANGUAGE}.jsonl"
-METRIC_OUTPUT_PATH="./results/results_${LANGUAGE}.jsonl"
+GENERATION_PATH=${3}
+GENERATION_PATH=${GENERATION_PATH:-"./results/generation_${LANGUAGE}.jsonl"}
+METRIC_OUTPUT_PATH=${4}
+CHAT_FORMAT=${5}
+CHAT_FORMAT=${CHAT_FORMAT:-"chatml"}
+METRIC_OUTPUT_PATH=${METRIC_OUTPUT_PATH:-"./results/results_${LANGUAGE}.jsonl"}
+TP=${6}
+TP=${TP:-1}
 PORT=$((RANDOM%9000+1000))
 echo "PORT: ${PORT}"
 
@@ -16,4 +24,7 @@ python evaluate.py  \
     --generation_path ${GENERATION_PATH} \
     --metric_output_path ${METRIC_OUTPUT_PATH} \
     --model_max_length 1024 \
-
+    --data_path ${DATA_PATH} \
+    --use_vllm \
+    --chat_format ${CHAT_FORMAT} \
+    --tensor_parallel_size ${TP}
