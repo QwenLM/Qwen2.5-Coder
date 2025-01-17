df0cfa20db2ee9b3b674b6c8ecfb98d020e93aad
zwpride <zwpride@gmail.com>
2025-01-17 14:24:57 +0800

    feat: add merge base model with all adapter ckpts and update README

--- finetuning/sft/merge_adapter.py
+++ finetuning/sft/merge_adapter.py
@@ -1,32 +1,41 @@
 from transformers import AutoTokenizer
 import argparse
 from peft import AutoPeftModelForCausalLM, PeftConfig
+import os
 
-def merge_model(args):
-    model_name_or_path = args.input_model_path
-    input_adapter_path = args.input_adapter_path
-    output_dir = args.output_path
+def merge_all_checkpoint_with_adapter(base_model_path, train_adapters_path, output_path):
+    for root, dirs, files in os.walk(train_adapters_path):
+        for dir in dirs:
+            if dir.startswith("checkpoint-"):
+                adapter_dir = os.path.join(root, dir)
+                merged_ckpt_dir = os.path.join(output_path, dir)
+                merge_adapter(base_model_path, adapter_dir, merged_ckpt_dir)
 
-    peft_config = PeftConfig.from_pretrained(input_adapter_path)
-    peft_config.base_model_name_or_path = model_name_or_path
+def merge_adapter(base_model_path, adapter_dir, output_path):
+    peft_config = PeftConfig.from_pretrained(adapter_dir)
+    peft_config.base_model_name_or_path = base_model_path
     peft_model = AutoPeftModelForCausalLM.from_pretrained(
-        input_adapter_path,
+        adapter_dir,
         config=peft_config,
     )
     merged_model = peft_model.merge_and_unload(progressbar=True)
-    merged_model.save_pretrained(output_dir)
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-    tokenizer.save_pretrained(output_dir)
+    merged_model.save_pretrained(output_path)
+    tokenizer = AutoTokenizer.from_pretrained(base_model_path)
+    tokenizer.save_pretrained(output_path)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Argument Parser Example')
-    parser.add_argument('--input_model_path', '-input_model_path', type=str, default="/cpfs01/data/shared/Group-m6/chuwei.zw/models/Qwen/Qwen2.5-Coder-1.5B", help='Path to model')
-    parser.add_argument('--input_adapter_path', '-input_adapter_path', type=str, default="/cpfs01/data/shared/Group-m6/chuwei.zw/models/checkpoints/1.5B/sft-test-version", help='Path to adapter folder')
+    parser.add_argument('--base_model_path', '-base_model_path', type=str, default="/cpfs01/data/shared/Group-m6/chuwei.zw/models/Qwen/Qwen2.5-Coder-1.5B", help='Path to model')
+    parser.add_argument('--train_adapters_path', '-train_adapters_path', type=str, default="/cpfs01/data/shared/Group-m6/chuwei.zw/models/checkpoints/1.5B/sft-test-version", help='Path to adapter folder')
     parser.add_argument('--output_path', '-output_path', type=str, default="/cpfs01/data/shared/Group-m6/chuwei.zw/models/checkpoints/1.5B/sft-test-version-merge", help='Path to output folder')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
     print(args)
-    merge_model(args)
+    # 最后一个再保存一下
+    merge_all_checkpoint_with_adapter(args.base_model_path, args.train_adapters_path, args.output_path)
+    # all
+    merge_adapter(args.base_model_path, args.train_adapters_path, args.output_path)
+    
+
